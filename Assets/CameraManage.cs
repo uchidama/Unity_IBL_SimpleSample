758db6c646ff40ad6ee7fb5c3e3be6717800443f
Shin Ishimaru <shinishimaru@gmail.com>
2017-11-01 02:37:51 +0900

    first commit

---
+++ Assets/CameraManage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManage : MonoBehaviour {
+
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = this.transform.position;
+		UpdatePos ();
+	}
+
+	void UpdatePos (){
+		float theta = Time.time * Mathf.PI/180*20;
+		transform.position = new Vector3 (startPos.magnitude * Mathf.Cos (theta), startPos.y, startPos.magnitude * Mathf.Sin (theta));
+		transform.LookAt(Vector3.zero);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdatePos ();
+	}
+}
